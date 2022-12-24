fbc54ec42802985a3f5b2ff0d620b250b3a43add
aghora7 <ukikieran@gmail.com>
2022-12-25 01:51:51 +0530

    fleur: Build required keymaster interface
    Signed-off-by: aghora7 <ukikieran@gmail.com>

--- device.mk
+++ device.mk
@@ -90,6 +90,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl \
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0.vendor
+    
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.fleur
